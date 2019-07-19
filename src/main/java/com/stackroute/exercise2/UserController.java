f9668b3275afda5ebe4bc380632dd5a201a9e10c
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 11:01:59 +0530

    Update UserController.java

--- src/main/java/com/stackroute/exercise2/UserController.java
+++ src/main/java/com/stackroute/exercise2/UserController.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
+    //Handlor method for the request that are ends with the login
     @RequestMapping(value="login")
     public ModelAndView welcome(Model model,@ModelAttribute("user") User user)
     {
+        //Creating object for model and view
         ModelAndView modelAndView=new ModelAndView();
+        //Adding attributes to the view
         model.addAttribute("user",user);
+        //Setting the view display to display the information
         modelAndView.setViewName("display");
+        //Returning modelandview object
         return modelAndView;
     }
 }
