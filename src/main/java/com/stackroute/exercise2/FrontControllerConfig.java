146272ce0a9c9ab0da641e31681a10a17a4ae724
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 10:58:49 +0530

    Update FrontControllerConfig.java

--- src/main/java/com/stackroute/exercise2/FrontControllerConfig.java
+++ src/main/java/com/stackroute/exercise2/FrontControllerConfig.java
@@ -1,18 +1,18 @@
 package com.stackroute.exercise2;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
+//This is the dispatcherservlet acts as the front controller
 public class FrontControllerConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    //Mapping the configuration class
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{Config.class};
     }
-
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[0];
     }
-
+    //Maps to the request has the / at the end
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
