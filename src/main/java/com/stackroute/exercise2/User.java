8dba5d9eed2b3de790263cf77a77cf51719ffaa1
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 11:00:02 +0530

    Update User.java

--- src/main/java/com/stackroute/exercise2/User.java
+++ src/main/java/com/stackroute/exercise2/User.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Component;
 public class User {
      private String username;
      private String password;
+     //Parameterized constructor
     public User(String name,String password) {
         this.username = name;
         this.password=password;
     }
-
+     //Default constructor
     public User() {
     }
-
+     //Getter and setter methods for the members of the class
     public String getUsername() {
         return username;
     }
